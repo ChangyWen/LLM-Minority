aecdebb528ba23d3122c28123d4bc72f8f3e993b
ChangyWen <ddcwen@gmail.com>
2025-11-12 10:46:21 +0800

    chore: new run

---
+++ src/prepare-data.py
@@ -0,0 +1,30 @@
+# from datasets import load_dataset
+
+# ds = load_dataset("opensporks/resumes")
+
+# for item in ds["train"]:
+#     idx = str(item["ID"])
+#     resume = item["Resume_str"]
+#     print(f"{idx}\n{resume}\n\n")
+#     input()
+
+import pandas as pd
+import json
+
+df = pd.read_csv("dataset/Resume.csv", encoding="utf-8")
+
+save_file = "dataset/resumes.jsonl"
+for index, row in df.iterrows():
+    idx = str(row["ID"]).strip()
+    job = row["Category"].strip()
+    resume = row["Resume_str"].strip()
+
+    if len(job) <= 0: continue
+    if len(resume) <= 500: continue
+
+    with open(save_file, "a") as f:
+        f.write(json.dumps({
+            "idx": idx,
+            "job": job,
+            "resume": resume
+        }) + "\n")
