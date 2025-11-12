4721cb664b961d3cdcb7871264c58b35e4412d46
ChangyWen <ddcwen@gmail.com>
2025-11-12 13:28:39 +0800

    chore: new run

--- run-rank.sh
+++ run-rank.sh
@@ -1,9 +1,10 @@
 set -x
 
-gender_order_id=$1
-total_count=$2
+gender=$1
+gender_order_id=$2
+total_count=$3
 
 for ((i=0; i<total_count; i++)); do
-    nohup python src/rank-resumes.py $gender_order_id >/dev/null 2>&1 &
+    nohup python src/rank-resumes-$gender.py $gender_order_id >/dev/null 2>&1 &
     sleep 0.1
 done
