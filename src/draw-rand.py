9c6e19607efc7053b11056f8d33bcacfcb1e54c6
ChangyWen <ddcwen@gmail.com>
2025-11-13 11:17:25 +0800

    chore: new run

---
+++ src/draw-rand.py
@@ -0,0 +1,51 @@
+import json
+
+
+if __name__ == "__main__":
+    # {"genders": ["Female", "Male", "Male", "Female", "Female"], "candidate_order": [1, 2, 3, 4, 0], "suggested_candidate_id": 2, "hit_candidate_id": 3}
+
+    same_gender_count_to_count = {}
+    same_gender_count_to_hit_count = {}
+
+    total_count = 0
+    with open("outputs/rand/consultant_samples.jsonl", "r") as f:
+        for line in f:
+            total_count += 1
+            item = json.loads(line)
+            genders = item["genders"]
+            candidate_order = item["candidate_order"]
+            suggested_candidate_id = item["suggested_candidate_id"]
+            hit_candidate_id = item["hit_candidate_id"]
+
+            for candidate_id in [0, 1, 2, 3, 4]:
+                same_gender_count = genders.count(genders[candidate_id]) - 1
+                if same_gender_count not in same_gender_count_to_count:
+                    same_gender_count_to_count[same_gender_count] = 0
+                same_gender_count_to_count[same_gender_count] += 1
+                if candidate_id == hit_candidate_id:
+                    if same_gender_count not in same_gender_count_to_hit_count:
+                        same_gender_count_to_hit_count[same_gender_count] = 0
+                    same_gender_count_to_hit_count[same_gender_count] += 1
+
+    same_gender_count_to_count = dict(sorted(same_gender_count_to_count.items(), key=lambda x: x[0]))
+    for same_gender_count, count in same_gender_count_to_count.items():
+        hit_count = 0
+        if same_gender_count in same_gender_count_to_hit_count:
+            hit_count = same_gender_count_to_hit_count[same_gender_count]
+        print(f"same_gender_count: {same_gender_count}, count: {count}, hit_rate: {(hit_count / count):.2f}")
+
+    #         if same_gender_count not in same_gender_count_to_candidate_to_count:
+    #             same_gender_count_to_candidate_to_count[same_gender_count] = {}
+    #         if hit_candidate_id not in same_gender_count_to_candidate_to_count[same_gender_count]:
+    #             same_gender_count_to_candidate_to_count[same_gender_count][hit_candidate_id] = 0
+    #         same_gender_count_to_candidate_to_count[same_gender_count][hit_candidate_id] += 1
+
+    # temp = 0
+    # for same_gender_count, candidate_to_count in same_gender_count_to_candidate_to_count.items():
+    #     print(f"same_gender_count: {same_gender_count}")
+    #     for candidate, count in candidate_to_count.items():
+    #         print(f"candidate: {candidate}, count: {count}")
+    #         temp += count
+
+    # print(f"total_count: {total_count}")
+    # print(f"temp: {temp}")
