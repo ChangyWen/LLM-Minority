01395b2b8792ee6234c2678952aa2099bab17b7e
ChangyWen <ddcwen@gmail.com>
2025-11-17 13:16:51 +0800

    chore: new run

--- run-societal.sh
+++ run-societal.sh
@@ -4,6 +4,6 @@ attribute_type=$1
 total_count=$2
 
 for ((i=0; i<total_count; i++)); do
-    nohup python src/societal.py $attribute_type >/dev/null 2>&1 &
+    nohup python src/societal.py "$attribute_type" >/dev/null 2>&1 &
     sleep 0.5
 done
