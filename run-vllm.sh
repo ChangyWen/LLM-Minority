d01cad196ac453f018fb81ea212dde2c4808790c
ChangyWen <ddcwen@gmail.com>
2025-11-20 08:47:55 +0800

    chore: new run

--- run-vllm.sh
+++ run-vllm.sh
@@ -1,19 +1,15 @@
 set -x
 
 ######### start vllm server #########
-vllm serve Qwen/Qwen3-Next-80B-A3B-Instruct \
+nohup vllm serve Qwen/Qwen3-Next-80B-A3B-Instruct \
   --tensor-parallel-size 8 \
-  --max-model-len 8192 \
-  --port 8000 &
+  --max-model-len 10240 \
+  --port 8000 >/dev/null 2>&1 &
 
 echo "*********** Waiting for vllm server to start ***********"
-sleep 360
+sleep 600
 echo "*********** Done waiting ***********"
 
-######### run the jobs in parallel #########
-NUM_JOBS=3   # change this to however many parallel runs you want
-
-for i in $(seq 1 $NUM_JOBS); do
-    echo "=== starting job $i ==="
-    python src/contextual.py Qwen/Qwen3-Next-80B-A3B-Instruct Gender 4 10 &
-done
+python src/contextual.py Qwen/Qwen3-Next-80B-A3B-Instruct Gender 4 10 &
+python src/contextual.py Qwen/Qwen3-Next-80B-A3B-Instruct Gender 4 10 &
+python src/contextual.py Qwen/Qwen3-Next-80B-A3B-Instruct Gender 4 10
