8c8f4551495bdd517d2e2aa3e48d28a8da339e56
ChangyWen <ddcwen@gmail.com>
2025-11-21 00:49:54 +0800

    chore: new run

--- run-societal.sh
+++ run-societal.sh
@@ -7,6 +7,6 @@ total_count=$4
 
 
 for ((i=0; i<total_count; i++)); do
-    nohup python src/contextual.py $model "$attribute_type" $resume_count $pool_count >/dev/null 2>&1 &
+    nohup python src/societal.py $model "$attribute_type" $resume_count $pool_count >/dev/null 2>&1 &
     sleep 10
 done
