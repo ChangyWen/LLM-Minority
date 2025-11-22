f76cf9136e093fad474861d7be728b8f6696081f
ChangyWen <ddcwen@gmail.com>
2025-11-22 23:41:00 +0800

    chore: enw run

---
+++ run-vllm-societal.sh
@@ -0,0 +1,25 @@
+set -x
+
+# model=Qwen/Qwen3-Next-80B-A3B-Instruct
+# model=meta-llama/Llama-3.3-70B-Instruct
+# model=openai/gpt-oss-120b
+model=$1
+
+######### start vllm server #########
+nohup vllm serve $model \
+  --trust-remote-code \
+  --tensor-parallel-size 8 \
+  --max-model-len 12288 \
+  --port 8000 >/dev/null 2>&1 &
+
+echo "*********** Waiting for vllm server to start ***********"
+sleep 600
+echo "*********** Done waiting ***********"
+
+python src/societal.py $model "Gender Identity" 5 200 &
+sleep 5
+python src/societal.py $model "Sexual Orientation" 5 200 &
+sleep 5
+python src/societal.py $model "Disability Status" 5 200 &
+sleep 5
+python src/societal.py $model "Chronic Health Condition Status" 5 200
