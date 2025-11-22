0ad178fc31dd8a2ded7475b3874b7c61160bd858
ChangyWen <ddcwen@gmail.com>
2025-11-22 23:42:20 +0800

    chore: enw run

--- run-vllm-societal.sh
+++ run-vllm-societal.sh
@@ -9,17 +9,17 @@ model=$1
 nohup vllm serve $model \
   --trust-remote-code \
   --tensor-parallel-size 8 \
-  --max-model-len 12288 \
+  --max-model-len 10240 \
   --port 8000 >/dev/null 2>&1 &
 
 echo "*********** Waiting for vllm server to start ***********"
 sleep 600
 echo "*********** Done waiting ***********"
 
-python src/societal.py $model "Gender Identity" 5 200 &
+python src/societal.py $model "Gender Identity" 200 &
 sleep 5
-python src/societal.py $model "Sexual Orientation" 5 200 &
+python src/societal.py $model "Sexual Orientation" 200 &
 sleep 5
-python src/societal.py $model "Disability Status" 5 200 &
+python src/societal.py $model "Disability Status" 200 &
 sleep 5
-python src/societal.py $model "Chronic Health Condition Status" 5 200
+python src/societal.py $model "Chronic Health Condition Status" 200
