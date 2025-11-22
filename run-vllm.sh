a648026e242e324298b79d82ceba2204ee4755ff
ChangyWen <ddcwen@gmail.com>
2025-11-22 09:11:53 +0800

    chore: new run

--- run-vllm.sh
+++ run-vllm.sh
@@ -1,8 +1,11 @@
 set -x
 
+# model=Qwen/Qwen3-Next-80B-A3B-Instruct
+# model=meta-llama/Llama-3.3-70B-Instruct
+model=openai/gpt-oss-120b
+
 ######### start vllm server #########
-# nohup vllm serve Qwen/Qwen3-Next-80B-A3B-Instruct \
-nohup vllm serve meta-llama/Llama-3.3-70B-Instruct \
+nohup vllm serve $model \
   --trust-remote-code \
   --tensor-parallel-size 8 \
   --max-model-len 12288 \
@@ -12,11 +15,8 @@ echo "*********** Waiting for vllm server to start ***********"
 sleep 600
 echo "*********** Done waiting ***********"
 
-# python src/contextual.py Qwen/Qwen3-Next-80B-A3B-Instruct Gender 5 200 &
-python src/contextual.py meta-llama/Llama-3.3-70B-Instruct Gender 5 200 &
+python src/contextual.py $model Gender 5 200 &
 sleep 5
-# python src/contextual.py Qwen/Qwen3-Next-80B-A3B-Instruct Gender 5 200 &
-python src/contextual.py meta-llama/Llama-3.3-70B-Instruct Gender 5 200 &
+python src/contextual.py $model Gender 5 200 &
 sleep 5
-# python src/contextual.py Qwen/Qwen3-Next-80B-A3B-Instruct Gender 5 200
-python src/contextual.py meta-llama/Llama-3.3-70B-Instruct Gender 5 200
+python src/contextual.py $model Gender 5 200
