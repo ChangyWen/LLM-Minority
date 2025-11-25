55f60dc5e671bebfb9a17eb0672d183fe8a81841
ChangyWen <ddcwen@gmail.com>
2025-11-25 21:51:50 +0800

    chore: new run

--- run-contextual.sh
+++ run-contextual.sh
@@ -1,13 +1,14 @@
 set -x
 
-model=$1
-attribute_type=$2
-resume_count=$3
-pool_count=$4
-total_count=$5
+application=$1
+model=$2
+attribute_type=$3
+resume_count=$4
+pool_count=$5
+total_count=$6
 
 
 for ((i=0; i<total_count; i++)); do
-    nohup python src/contextual.py $model "$attribute_type" $resume_count $pool_count >/dev/null 2>&1 &
+    nohup python src/${application}/contextual.py $model "$attribute_type" $resume_count $pool_count >/dev/null 2>&1 &
     sleep 10
 done
