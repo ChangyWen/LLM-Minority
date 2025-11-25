14d4372334182233fcea528741cd254b670b2294
ChangyWen <ddcwen@gmail.com>
2025-11-25 21:54:02 +0800

    chore: new run

--- run-vllm-contextual.sh
+++ run-vllm-contextual.sh
@@ -3,8 +3,9 @@ set -x
 # model=Qwen/Qwen3-Next-80B-A3B-Instruct
 # model=meta-llama/Llama-3.3-70B-Instruct
 # model=openai/gpt-oss-120b
-model=$1
-attribute_type=$2
+application=$1
+model=$2
+attribute_type=$3
 
 ######### start vllm server #########
 nohup vllm serve $model \
@@ -17,8 +18,18 @@ echo "*********** Waiting for vllm server to start ***********"
 sleep 600
 echo "*********** Done waiting ***********"
 
-python src/contextual.py $model "$attribute_type" 5 200 &
+
+if [ "$application" == "hiring" ]; then
+  pool_count=200
+elif [ "$application" == "loan" ]; then
+  pool_count=500
+else
+  echo "Invalid application: $application"
+  exit 1
+fi
+
+python src/${application}/contextual.py $model "$attribute_type" 5 $pool_count &
 sleep 5
-python src/contextual.py $model "$attribute_type" 5 200 &
+python src/${application}/contextual.py $model "$attribute_type" 5 $pool_count &
 sleep 5
-python src/contextual.py $model "$attribute_type" 5 200
+python src/${application}/contextual.py $model "$attribute_type" 5 $pool_count
