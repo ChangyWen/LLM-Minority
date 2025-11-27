c81618242f3e3ca25d3cd4a0f742fc469648daf0
ChangyWen <ddcwen@gmail.com>
2025-11-28 00:59:22 +0800

    chore: new run

--- run-societal.sh
+++ run-societal.sh
@@ -1,12 +1,12 @@
 set -x
 
-model=$1
-attribute_type=$2
-pool_count=$3
+application=$1
+model=$2
+attribute_type=$3
 total_count=$4
 
 
 for ((i=0; i<total_count; i++)); do
-    nohup python src/societal.py $model "$attribute_type" $resume_count $pool_count >/dev/null 2>&1 &
+    nohup python src/$application/societal.py $model "$attribute_type" >/dev/null 2>&1 &
     sleep 10
 done
