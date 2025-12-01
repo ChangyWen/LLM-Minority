563f7c7afed49ebcf8d08a549782cfecbfe36706
ChangyWen <ddcwen@gmail.com>
2025-12-01 11:51:43 +0800

    chore: new run

--- src/edu/contextual.py
+++ src/edu/contextual.py
@@ -69,6 +69,14 @@ def complete(prompt, model_name="msra-gpt-5", reasoning_effort_or_thinking_budge
             temperature = 1.0
         elif model_name == "zai-org/GLM-4.5-Air":
             temperature = 0.6
+        elif model_name == "nvidia/Llama-3_3-Nemotron-Super-49B-v1_5":
+            temperature = 0.6
+        elif model_name == "ByteDance-Seed/Seed-OSS-36B-Instruct":
+            temperature = 1.1
+        elif model_name == "Qwen/Qwen3-235B-A22B-Instruct-2507":
+            temperature = 0.7
+        elif model_name == "nvidia/NVIDIA-Nemotron-Nano-12B-v2":
+            temperature = 0.6
         else:
             print(f"Model name {model_name} not supported")
             raise ValueError(f"Model name {model_name} not supported")
