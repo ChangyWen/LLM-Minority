be5df25434e54ee651170b495922e6949a64ca8b
ChangyWen <ddcwen@gmail.com>
2025-12-01 22:37:00 +0800

    chore: new run

--- src/loan/societal.py
+++ src/loan/societal.py
@@ -143,16 +143,16 @@ if __name__ == "__main__":
 
 
     if "msra" in model_name:
-        os.makedirs(f"outputs/edu/societal/{attribute_type}", exist_ok=True)
+        os.makedirs(f"outputs/loan/societal/{attribute_type}", exist_ok=True)
         sub_model_name = model_name.split("/")[-1]
-        save_file = f"outputs/edu/societal/{attribute_type}/{sub_model_name}.jsonl"
-        dataset_dir = "dataset/edu"
+        save_file = f"outputs/loan/societal/{attribute_type}/{sub_model_name}.jsonl"
+        dataset_dir = "dataset/loan"
     else:
-        os.makedirs(f"/mnt/blob_output/v-dachengwen/LLM-Minority/outputs/edu/societal/{attribute_type}", exist_ok=True)
+        os.makedirs(f"/mnt/blob_output/v-dachengwen/LLM-Minority/outputs/loan/societal/{attribute_type}", exist_ok=True)
         sub_model_name = model_name.split("/")[-1]
         ts = int(time.time() * 1000)
-        save_file = f"/mnt/blob_output/v-dachengwen/LLM-Minority/outputs/edu/societal/{attribute_type}/{sub_model_name}_ts{ts}_rd{random.randint(1, 1000000)}.jsonl"
-        dataset_dir = "/mnt/blob_output/v-dachengwen/LLM-Minority/dataset/edu"
+        save_file = f"/mnt/blob_output/v-dachengwen/LLM-Minority/outputs/loan/societal/{attribute_type}/{sub_model_name}_ts{ts}_rd{random.randint(1, 1000000)}.jsonl"
+        dataset_dir = "/mnt/blob_output/v-dachengwen/LLM-Minority/dataset/loan"
 
     total_query_time = 0
     total_failed_time = 0
@@ -164,6 +164,9 @@ if __name__ == "__main__":
 
         for attribute in attributes_list:
             prompt = get_prompt(sampled_candidate, attribute)
+            print(prompt)
+            input()
+            continue
 
             try:
                 start_time = time.time()
