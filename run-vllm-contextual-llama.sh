91f1b88ac5cbe89744dc244f7bd8a0d7737a277a
ChangyWen <ddcwen@gmail.com>
2026-01-19 12:58:26 +0000

    contextual llama

---
+++ run-vllm-contextual-llama.sh
@@ -0,0 +1,37 @@
+set -x
+
+application=$1
+model=$2
+attribute_type=$3
+gpu_count=$4
+
+
+if [ "$application" == "hiring" ]; then
+  pool_count=200
+  max_model_len=12288
+elif [ "$application" == "loan" ] || [ "$application" == "edu" ]; then
+  pool_count=500
+  max_model_len=5120
+else
+  echo "Invalid application: $application"
+  exit 1
+fi
+
+
+######### start vllm server #########
+nohup vllm serve $model \
+  --trust-remote-code \
+  --tensor-parallel-size $gpu_count \
+  --max-model-len $max_model_len \
+  --port 8000 >/dev/null 2>&1 &
+
+echo "*********** Waiting for vllm server to start ***********"
+sleep 300
+echo "*********** Done waiting ***********"
+
+
+python src/$application/contextual.py $model "$attribute_type" $context_size $pool_count $disable_thinking &
+sleep 5
+python src/$application/contextual.py $model "$attribute_type" $context_size $pool_count $disable_thinking &
+sleep 5
+python src/$application/contextual.py $model "$attribute_type" $context_size $pool_count $disable_thinking
